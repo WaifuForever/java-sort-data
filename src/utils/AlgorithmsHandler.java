19f1fbc622f50c0cdf10c09fb9de4d81d1873866
berrytern <berrytern@pop-os.localdomain>
2022-07-06 00:31:40 -0300

    feat: prepare dates to sort as integer.

--- src/utils/AlgorithmsHandler.java
+++ src/utils/AlgorithmsHandler.java
@@ -20,40 +20,47 @@ public class AlgorithmsHandler {
     private static Sorter[] sorters = new Sorter[] { new Counting() };
 
     private static void routine(Sorter sorter, CustomArray<String> data) {
-        int size = 10;
+        int size = 1000;
         long time[] = new long[size];
 
         for (int i = 0; i < size; i++) {
             data.shuffleArray(1);
 
-            Integer[] tags = tagHandler.getTagsArray(data, 1);
+            Integer[] tags = tagHandler.getNumberTagsFromArray(data, 1, 0);
 
             long startTime = System.nanoTime();
             sorter.sortArray(tags);
             long endTime = System.nanoTime();
 
-            data = tagHandler.reorderArray(tags, data, 1);
+            data = tagHandler.reorderArray(tags, data, 1, 0, false);
             time[i] = endTime - startTime;
         }
-        System.out.println(averageTime(time));
-        fh.write(sorter.getClass().getSimpleName() + ".csv", data, 0);
+        //System.out.println(averageTime(time, 3));
+        ArrayHandler.printArray(averageTime(time, 3));
+        fh.write(/*"password_length" + "wostcase"*/ sorter.getClass().getSimpleName() + ".csv", data, 0);
 
     }
 
-    private static long averageTime(long[] array) {
-        long sum = 0;
+    private static long[] averageTime(long[] array, int minor_steps) {
+        long[] media = {0, 0};
         for (int i = 0; i < array.length; i++) {
-            sum += array[i];
+            if(i<minor_steps){
+                media[0] += array[i];
+            }
+            media[1] += array[i];
         }
-        return sum / array.length;
+        if(minor_steps<array.length){
+            media[0] = media[0]/minor_steps;
+        }else{
+            media[0] = media[0]/array.length;
+        }
+        media[1]=media[1]/array.length;
+        return media;
     }
 
     public static void sortData(CustomArray<String> data) {
-        for (int j = 0; j < 200; j++){
-            for (int i = 0; i < sorters.length; i++) {
-                routine(sorters[i], data);
-            }
+        for (int i = 0; i < sorters.length; i++) {
+            routine(sorters[i], data);
         }
-
     }
 }
--- src/utils/TagHandler.java
+++ src/utils/TagHandler.java
@@ -2,38 +2,47 @@ package src.utils;
 
 public class TagHandler {
 
-    public CustomArray<String> reorderArray(Integer tags[], CustomArray<String> data, int skip) {
+    public CustomArray<String> reorderArray(Integer tags[], CustomArray<String> data, int skip, int index, boolean isString) {
         CustomArray<String> newCustomArray = new CustomArray<String>(data.getSize());
-
-        Integer[] oldTags = getTagsArray(data, skip);
-
-        for (int i = 0; i < skip; i++) {
-            newCustomArray.add(data.get(i));
+        if(isString){
+            String[] oldTags = getStringTagsFromArray(data, skip, index);
+            return newCustomArray;
+        }else{
+            Integer[] oldTags = getNumberTagsFromArray(data, skip, index);
+            for (int i = 0; i < skip; i++) {
+                newCustomArray.add(data.get(i));
+            }
+            for (int i = 0; i < tags.length; i++) {
+                newCustomArray.add(data.get(ArrayHandler.sequentialSearch(oldTags, tags[i]) + skip));
+            }
+            return newCustomArray;
         }
-        for (int i = 0; i < tags.length; i++) {
-            newCustomArray.add(data.get(ArrayHandler.sequentialSearch(oldTags, tags[i]) + skip));
-        }
-
-        //ArrayHandler.printArray(tags);
-        //System.out.printf("tags = %d, data = %d\n", tags.length, newCustomArray.getSize());
-        /*
-         * for (int i = 0; i < newCustomArray.getSize(); i++) {
-         * System.out.println(newCustomArray.get(i));
-         * }
-         */
-
-        return newCustomArray;
     }
 
-    public Integer[] getTagsArray(CustomArray<String> data, int index) {
-        int size = data.getSize() - index;
+
+    public Integer[] getStringTagsFromArray(CustomArray<String> data, int skip, int index) {
+        if( index < 0 || index > 5) index = 0;
+        int size = data.getSize() - skip;
         Integer[] arr = new Integer[size];
 
         for (int i = 0; i < size; i++) {
-            // System.out.printf("i = %d, index = %d\n", i, index);
+            String[] date = data.get(i + skip).split(",")[index].split("/", 3);
+            arr[i] = Integer.parseInt(date[2].concat(date[1]).concat(date[0]));
+        }
 
-            // System.out.println(data.get(i + index));
-            arr[i] = Integer.parseInt(data.get(i + index).split(",")[0]);
+        return arr;
+    }
+
+    public Integer[] getNumberTagsFromArray(CustomArray<String> data, int skip, int index) {
+        if(index<0 || index>5) index=0;
+        int size = data.getSize() - skip;
+        Integer[] arr = new Integer[size];
+
+        for (int i = 0; i < size; i++) {
+            // System.out.printf("i = %d, skip = %d\n", i, skip);
+
+            // System.out.println(data.get(i + skip));
+            arr[i] = Integer.parseInt(data.get(i + skip).split(",")[index]);
         }
 
         return arr;
