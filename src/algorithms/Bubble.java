28a0ca522afd1f814fbc0c1a703670450908606c
Waifu Forever <itachiut1r4@gmail.com>
2022-07-08 21:42:40 -0300

    refactor: Bubble uses the arrayhandler

--- src/algorithms/Bubble.java
+++ src/algorithms/Bubble.java
@@ -4,20 +4,17 @@ import src.interfaces.Sorter;
 import src.utils.ArrayHandler;
 
 public class Bubble implements Sorter {
-    private void swap(int[] arr, int i, int j){
-        arr[i] = (arr[i] + arr[j]) - (arr[j] = arr[i]);
-    }
 
-    private void bubbleSort(int[] arr){
-        for(int i=0; i < arr.length-1; i++){
-            for(int j=0; j < arr.length-1; j++){
-                if(arr[j] > arr[j+1])
-                    swap(arr, j, j+1);
+    private void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1])
+                    ArrayHandler.swap(arr, j, j + 1);
             }
         }
     }
 
-    public void sortArray(int[] arr){
+    public void sortArray(int[] arr) {
         bubbleSort(arr);
     }
 
