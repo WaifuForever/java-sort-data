15c8f8650071873cd579fb6be4a16b92a5b7e2d5
Waifu Forever <itachiut1r4@gmail.com>
2022-07-08 22:02:43 -0300

    fix: Quicksort

--- src/algorithms/Quick.java
+++ src/algorithms/Quick.java
@@ -5,28 +5,48 @@ import src.interfaces.Sorter;
 import src.utils.ArrayHandler;
 
 public class Quick implements Sorter {
-    private int partition(int[] arr, int left, int right) {
-        int p = arr[left];
-        int i = left + 1;
-        int j = right;
+    // method to find the partition position
+    private int partition(int array[], int low, int high) {
 
-        while (i <= j) {
-            if (arr[i] <= p)
+        // choose the rightmost element as pivot
+        int pivot = array[high];
+
+        // pointer for greater element
+        int i = (low - 1);
+
+        // traverse through all elements
+        // compare each element with pivot
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+
+                // if element smaller than pivot is found
+                // swap it with the greater element pointed by i
                 i++;
-            else if (arr[j] > p)
-                j--;
-            else
-                ArrayHandler.swap(arr, i, j);
+
+                // swapping element at i with element at j
+                ArrayHandler.swap(array, i, j);
+
+            }
+
         }
-        ArrayHandler.swap(arr, left, j);
-        return j;
+
+        // swapt the pivot element with the greater element specified by i
+        ArrayHandler.swap(array, i + 1, high);
+
+        // return the position from where partition is done
+        return (i + 1);
     }
 
-    private void quickSort(int[] arr, int left, int right) {
-        if (left < right) {
-            int pivot = partition(arr, left, right);
-            quickSort(arr, left, pivot - 1);
-            quickSort(arr, pivot + 1, right);
+    private void quickSort(int array[], int low, int high) {
+        if (low < high) {
+
+            int pi = partition(array, low, high);
+
+            // recursive call on the left of pivot
+            quickSort(array, low, pi - 1);
+
+            // recursive call on the right of pivot
+            quickSort(array, pi + 1, high);
         }
     }
 
